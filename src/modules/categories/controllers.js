dd911a50d5026d37f1a3e8dff28a3209f1dc157c
WilliamArceA <william.arce2118@gmail.com>
2024-04-12 17:45:05 -0400

    Added SwaggerUi support for categories, location and log modules

--- src/modules/categories/controllers.js
+++ src/modules/categories/controllers.js
@@ -6,6 +6,27 @@ const {
     deleteCategory,
 } = require('./model')
 
+/**
+ * @swagger
+ * tags:
+ *   name: Categories
+ *   description: Endpoints relacionados con las categorias de los activos
+ */
+
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     tags: [Categories]
+ *     summary: Obtiene todas las categorías.
+ *     description: Retorna una lista de todas las categorías disponibles.
+ *     responses:
+ *       '200':
+ *         description: Respuesta exitosa. Devuelve la lista de categorías.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
+
 async function GET(req, res) {
     try {
         const response = await getCategory()
@@ -15,6 +36,27 @@ async function GET(req, res) {
         return res.status(500).json({ errorCode: error.code, msg: error.message })
     }
 }
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *   get:
+ *     tags: [Categories]
+ *     summary: Obtiene una categoría por su ID.
+ *     description: Retorna una categoría específica según su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la categoría a obtener.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: Respuesta exitosa. Devuelve la categoría solicitada.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
 
 async function SHOW(req, res) {
     try {
@@ -28,6 +70,33 @@ async function SHOW(req, res) {
     }
 }
 
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     tags: [Categories]
+ *     summary: Crea una nueva categoría.
+ *     description: Crea una nueva categoría con el nombre proporcionado.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               cat_name:
+ *                 type: string
+ *                 description: Nombre de la categoría a crear.
+ *     responses:
+ *       '200':
+ *         description: Categoría creada con éxito.
+ *       '400':
+ *         description: Falta colocar atributos.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
+
+
 async function POST(req, res) {
     try {
         const { cat_name } = req.body
@@ -39,6 +108,39 @@ async function POST(req, res) {
         return res.status(500).json({ errorCode: error.code, msg: error.message })
     }
 }
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *   put:
+ *     tags: [Categories]
+ *     summary: Actualiza una categoría existente.
+ *     description: Actualiza el nombre de una categoría existente.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la categoría a actualizar.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               cat_name:
+ *                 type: string
+ *                 description: Nuevo nombre de la categoría.
+ *     responses:
+ *       '200':
+ *         description: Categoría actualizada con éxito.
+ *       '400':
+ *         description: Falta colocar atributos.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
 
 async function PUT(req, res) {
     try {
@@ -53,6 +155,27 @@ async function PUT(req, res) {
         return res.status(500).json({ errorCode: error.code, msg: error.message })
     }
 }
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *   delete:
+ *     tags: [Categories]
+ *     summary: Elimina una categoría existente.
+ *     description: Cambia el flag de una categoria y esta deja de ser visible por metodos tradicionales.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la categoría a eliminar.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: Categoría eliminada con éxito.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
 
 async function DELETE(req, res) {
     try {
--- src/modules/location/controllers.js
+++ src/modules/location/controllers.js
@@ -6,6 +6,27 @@ const {
     deleteLocation,
 } = require('./model')
 
+/**
+ * @swagger
+ * tags:
+ *   name: Location
+ *   description: Endpoints relacionados con las ubicaciones
+ */
+
+/**
+ * @swagger
+ * /location:
+ *   get:
+ *     tags: [Location]
+ *     summary: Obtiene todas las ubicaciones.
+ *     description: Retorna todas las ubicaciones disponibles.
+ *     responses:
+ *       '200':
+ *         description: Respuesta exitosa. Devuelve la lista de ubicaciones.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
+
 async function GET(req, res) {
     try {
         const response = await getLocation()
@@ -15,6 +36,27 @@ async function GET(req, res) {
         return res.status(500).json({ errorCode: error.code, msg: error.message })
     }
 }
+
+/**
+ * @swagger
+ * /location/{id}:
+ *   get:
+ *     tags: [Location]
+ *     summary: Obtiene una ubicación por su ID.
+ *     description: Retorna una ubicación específica según su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la ubicación a obtener.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: Respuesta exitosa. Devuelve la ubicación solicitada.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
 
 async function SHOW(req, res) {
     try {
@@ -28,14 +70,43 @@ async function SHOW(req, res) {
     }
 }
 
+/**
+ * @swagger
+ * /location:
+ *   post:
+ *     tags: [Location]
+ *     summary: Crea una nueva ubicación.
+ *     description: Crea una nueva ubicación con el nombre y el detalle proporcionados.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               location_name:
+ *                 type: string
+ *                 description: Nombre de la ubicación.
+ *               location_detail:
+ *                 type: string
+ *                 description: Detalles de la ubicación.
+ *     responses:
+ *       '200':
+ *         description: Ubicación creada con éxito.
+ *       '400':
+ *         description: Falta colocar atributos.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
+
 async function POST(req, res) {
     try {
-        const { name, detail } = req.body
-        if (!name || !detail) 
+        const { location_name, location_detail } = req.body
+        if (!location_name || !location_detail) 
         return res.status(400).json({ msg: 'Complete todos los datos' })
-        if (name == '' || detail =='') 
+        if (location_name == '' || location_detail =='') 
         return res.status(400).json({ msg: 'Complete todos los datos' })
-        const response = await createLocation(name,detail)
+        const response = await createLocation(location_name,location_detail)
         
         return res.status(200).json('Location created')
     } catch (error) {
@@ -43,21 +114,78 @@ async function POST(req, res) {
     }
 }
 
+/**
+ * @swagger
+ * /location/{id}:
+ *   put:
+ *     tags: [Location]
+ *     summary: Actualiza una ubicación existente.
+ *     description: Actualiza una ubicación existente según su ID con los nuevos datos proporcionados.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la ubicación a actualizar.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               location_name:
+ *                 type: string
+ *                 description: Nuevo nombre de la ubicación.
+ *               location_detail:
+ *                 type: string
+ *                 description: Nuevos detalles de la ubicación.
+ *     responses:
+ *       '200':
+ *         description: Ubicación actualizada con éxito.
+ *       '400':
+ *         description: Falta colocar atributos.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
+
 async function PUT(req, res) {
     try {
         const loc_id = req.params.id
-        const { name, detail } = req.body
-        if (!name || !detail) 
+        const { location_name, location_detail } = req.body
+        if (!location_name || !location_detail) 
         return res.status(400).json({ msg: 'Complete todos los datos' })
-        if (name == '' || detail == '') 
+        if (location_name == '' || location_detail == '') 
         return res.status(400).json({ msg: 'Complete todos los datos' })
-        await updateLocation(loc_id, name, detail)
+        await updateLocation(loc_id, location_name, location_detail)
 
         return res.status(200).json('Location updated')
     } catch (error) {
         return res.status(500).json({ errorCode: error.code, msg: error.message })
     }
 }
+
+/**
+ * @swagger
+ * /location/{id}:
+ *   delete:
+ *     tags: [Location]
+ *     summary: Elimina una ubicación por su ID.
+ *     description: Cambia el flag de una ubicacion y esta deja de ser visible por metodos tradicionales.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la ubicación a eliminar.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: Ubicación eliminada con éxito.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
 
 async function DELETE(req, res) {
     try {
